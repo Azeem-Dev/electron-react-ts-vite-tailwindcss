e4b81c0e54de09af8c7d904810402843ef8fc16a
Azeem Sharif <azeemsharif5822@gmail.com>
2025-02-19 19:01:51 +0500

    initial

---
+++ src/preload/index.ts
@@ -0,0 +1,17 @@
+import { contextBridge } from 'electron'
+import { electronAPI } from '@electron-toolkit/preload'
+
+
+if (!process.contextIsolated)
+  throw new Error('contextIsolation must be enabled in the BrowserWinodw')
+
+const context = {
+  //TODO:
+}
+
+try {
+  contextBridge.exposeInMainWorld('electron', electronAPI)
+  contextBridge.exposeInMainWorld('context', context)
+} catch (error) {
+  console.error(error)
+}
---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,11 @@
+
+
+function App(): JSX.Element {
+  // const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+
+  return (
+    <div className="bg-blue-300 text-white p-4 cursor hover:text-[20px]">Hello, Tailwind CSS!</div>
+  )
+}
+
+export default App
