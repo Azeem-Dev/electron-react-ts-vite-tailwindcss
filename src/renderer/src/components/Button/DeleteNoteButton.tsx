e0aefd4c331bfc76aacc96f57febe6cb997f7c70
Azeem Sharif <azeemsharif5822@gmail.com>
2025-02-21 15:11:55 +0500

    added create and delete

--- src/renderer/src/components/Button/DeleteNoteButton.tsx
+++ src/renderer/src/components/Button/DeleteNoteButton.tsx
@@ -1,9 +1,17 @@
 import { ActionButton, ActionButtonProps } from '@/components'
+import { deleteNotAtom } from '@/store'
+import { useSetAtom } from 'jotai'
 import { FaRegTrashCan } from 'react-icons/fa6'
 
 export const DeleteNoteButton = ({ ...props }: ActionButtonProps): JSX.Element => {
+  const deleteNote = useSetAtom(deleteNotAtom)
+
+  const handleDelete = ():void => {
+    deleteNote()
+  }
+
   return (
-    <ActionButton {...props}>
+    <ActionButton onClick={handleDelete} {...props}>
       <FaRegTrashCan className="w-4 h-4 text-zinc-300" />
     </ActionButton>
   )
--- src/renderer/src/components/Button/NewNoteButton.tsx
+++ src/renderer/src/components/Button/NewNoteButton.tsx
@@ -1,9 +1,16 @@
 import { ActionButton, ActionButtonProps } from '@/components'
+import { createEmptyNoteAtom } from '@/store'
+import { useSetAtom } from 'jotai'
 import { AiFillFileAdd } from 'react-icons/ai'
 
 export const NewNoteButton = ({ ...props }: ActionButtonProps): JSX.Element => {
+  const createEmptyNote = useSetAtom(createEmptyNoteAtom)
+
+  const handleCreation = ():void => {
+    createEmptyNote()
+  }
   return (
-    <ActionButton {...props}>
+    <ActionButton onClick={handleCreation} {...props} >
       <AiFillFileAdd className="w-4 h-4 text-zinc-300" />
     </ActionButton>
   )
